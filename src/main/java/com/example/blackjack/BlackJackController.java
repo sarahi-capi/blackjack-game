b10b40fe5618cfcb44d9fed114086efcb2aade28
Sarahi Capi Casillas <s240349@th-ab.de>
2024-12-03 16:06:20 +0100

    Bet buttons working

--- src/main/java/com/example/blackjack/BlackJackController.java
+++ src/main/java/com/example/blackjack/BlackJackController.java
@@ -29,8 +29,8 @@ public class BlackJackController {
     @FXML
     private Label youBetText; // To show how much money the user is betting
 
-    private int minimumBet; // To set a minimum bet
-    private int yourMoney; // How much money the user has
+    private int minimumBet = 10; // To set a minimum bet
+    private int yourMoney = 200; // How much money the user has
     private int betMoney; // How much money the user is betting
 
 
@@ -54,7 +54,13 @@ public class BlackJackController {
     void goPlayGameScreen(MouseEvent event) {
         gameScreen.setVisible(true);
         placeBetScreen.setVisible(true);
+        betMoney = minimumBet;
+        yourMoney -= 10;
+        updateQuantities();
+    }
 
+    // Update "Your Money" and "Your bet" texts
+    void updateQuantities() {
         youHaveText.setText("You have: " + yourMoney);
         youBetText.setText("Your bet: " + betMoney);
     }
@@ -68,52 +74,81 @@ public class BlackJackController {
     // Add 10 to the bet
     @FXML
     void plusButton10(MouseEvent event) {
-        if (yourMoney > 10) {
+        if (yourMoney >= 10) {
             betMoney += 10;
             yourMoney -= 10;
+            updateQuantities();
         }
     }
 
     // Subtract 10 from the bet
     @FXML
     void minusButton10(MouseEvent event) {
-        if (yourMoney == min)
+        if (betMoney - 10 >= minimumBet) {
+            betMoney -= 10;
+            yourMoney += 10;
+            updateQuantities();
+        }
     }
 
     // Add 20 to the bet
     @FXML
     void plusButton20(MouseEvent event) {
-
+        if (yourMoney >= 20) {
+            betMoney += 20;
+            yourMoney -= 20;
+            updateQuantities();
+        }
     }
 
     // Subtract 20 from the bet
     @FXML
     void minusButton20(MouseEvent event) {
-
+        if (betMoney - 20 >= minimumBet) {
+            betMoney -= 20;
+            yourMoney += 20;
+            updateQuantities();
+        }
     }
 
     // Add 50 to the bet
     @FXML
     void plusButton50(MouseEvent event) {
-
+        if (yourMoney >= 50) {
+            betMoney += 50;
+            yourMoney -= 50;
+            updateQuantities();
+        }
     }
 
     // Subtract 50 from the bet
     @FXML
     void minusButton50(MouseEvent event) {
-
+        if (betMoney - 50 >= minimumBet) {
+            betMoney -= 50;
+            yourMoney += 50;
+            updateQuantities();
+        }
     }
 
     // Add 200 to the bet
     @FXML
     void plusButton200(MouseEvent event) {
-
+        if (yourMoney >= 200) {
+            betMoney += 200;
+            yourMoney -= 200;
+            updateQuantities();
+        }
     }
 
     // Subtract 200 from the bet
     @FXML
     void minusButton200(MouseEvent event) {
-
+        if (betMoney - 200 >= minimumBet) {
+            betMoney -= 200;
+            yourMoney += 200;
+            updateQuantities();
+        }
     }
 
 }

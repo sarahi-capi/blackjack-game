c1dd448fb29890a06c8d27b2c8ca14a1e42a575f
Sarahi Capi Casillas <s240349@th-ab.de>
2024-12-03 11:23:19 +0100

    Renaming files

--- src/main/java/com/example/blackjack/BlackJackController.java
+++ src/main/java/com/example/blackjack/BlackJackController.java
@@ -3,7 +3,7 @@ package com.example.blackjack;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class BlackJackController {
     @FXML
     private Label welcomeText;
 

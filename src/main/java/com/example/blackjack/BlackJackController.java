80ab459f61eb81efd7bdb045c968c963b21f00eb
Sarahi Capi Casillas <s240349@th-ab.de>
2024-12-03 18:37:24 +0100

    Finished UI, working on code

--- src/main/java/com/example/blackjack/BlackJackController.java
+++ src/main/java/com/example/blackjack/BlackJackController.java
@@ -1,11 +1,13 @@
 package com.example.blackjack;
 
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 
 public class BlackJackController {
 
@@ -15,9 +17,9 @@ public class BlackJackController {
     @FXML
     private AnchorPane howToPlayScreen; // "How to Play" Pane
     @FXML
-    private AnchorPane gameScreen; // "Game" Pane
-    @FXML
     private AnchorPane placeBetScreen; // "Place Bet" Pane
+    @FXML
+    private AnchorPane gameScreen; // "Game" Pane
 
     // Tutorial variables
     @FXML
@@ -25,13 +27,154 @@ public class BlackJackController {
 
     // Place Bet variables
     @FXML
-    private Label youHaveText; // To show how much money the user has
+    private Label youHaveText; // To show how much money the player has
     @FXML
-    private Label youBetText; // To show how much money the user is betting
+    private Label youBetText; // To show how much money the player is betting
 
-    private int minimumBet = 10; // To set a minimum bet
-    private int yourMoney = 200; // How much money the user has
-    private int betMoney; // How much money the user is betting
+    private final int minimumBet = 10; // To set a minimum bet
+    private int yourMoney = 200; // How much money the player has
+    private int betMoney; // How much money the player is betting
+
+    // Game variables
+    @FXML
+    private StackPane nextButton; // To have access to "Next" button
+    @FXML
+    private Text winOrLoseMessage; // A win or lose text
+
+    // Variables to change player's Card Club
+    @FXML
+    private TextArea playerCardClub1;
+    @FXML
+    private TextArea playerCardClub2;
+    @FXML
+    private TextArea playerCardClub3;
+    @FXML
+    private TextArea playerCardClub4;
+    @FXML
+    private TextArea playerCardClub5;
+    @FXML
+    private TextArea playerCardClub6;
+    @FXML
+    private TextArea playerCardClub7;
+    @FXML
+    private TextArea playerCardClub8;
+
+    // Variables to change player's Card Value
+    @FXML
+    private Text playerCardValue1;
+    @FXML
+    private Text playerCardValue2;
+    @FXML
+    private Text playerCardValue3;
+    @FXML
+    private Text playerCardValue4;
+    @FXML
+    private Text playerCardValue5;
+    @FXML
+    private Text playerCardValue6;
+    @FXML
+    private Text playerCardValue7;
+    @FXML
+    private Text playerCardValue8;
+
+    // Variables to change dealer's Card Club
+    @FXML
+    private TextArea dealerCardClub1;
+    @FXML
+    private TextArea dealerCardClub2;
+    @FXML
+    private TextArea dealerCardClub3;
+    @FXML
+    private TextArea dealerCardClub4;
+    @FXML
+    private TextArea dealerCardClub5;
+    @FXML
+    private TextArea dealerCardClub6;
+    @FXML
+    private TextArea dealerCardClub7;
+    @FXML
+    private TextArea dealerCardClub8;
+
+    // Variables to change dealer's Card Value
+    @FXML
+    private Text dealerCardValue1;
+    @FXML
+    private Text dealerCardValue2;
+    @FXML
+    private Text dealerCardValue3;
+    @FXML
+    private Text dealerCardValue4;
+    @FXML
+    private Text dealerCardValue5;
+    @FXML
+    private Text dealerCardValue6;
+    @FXML
+    private Text dealerCardValue7;
+    @FXML
+    private Text dealerCardValue8;
+
+    // Clubs
+
+    // Diamond Club
+    String diamondClub =
+                            "                         \n" +
+                            "                         \n" +
+                            "           :%%+          \n" +
+                            "          *%%%%%         \n" +
+                            "        -%%%%%%%%+       \n" +
+                            "       #%%%%%%%%%%#      \n" +
+                            "     :%%%%%%%%%%%%%%-    \n" +
+                            "     .%%%%%%%%%%%%%%.    \n" +
+                            "       +%%%%%%%%%%*      \n" +
+                            "        .%%%%%%%%:       \n" +
+                            "          =%%%%+         \n" +
+                            "            ##           \n";
+
+    // Clover Club
+    String cloverClub =
+                            "                          \n" +
+                            "                          \n" +
+                            "         .*%%%%%*         \n" +
+                            "        *%%%%%%%%%        \n" +
+                            "        *%%%%%%%%%        \n" +
+                            "         #%%%%%%%-        \n" +
+                            "    #%%%%%%%%%%%%%%%%%+   \n" +
+                            "   %%%%%%%%%%%%%%%%%%%%%  \n" +
+                            "  +%%%%%%%%%%%%%%%%%%%%%  \n" +
+                            "    =#%%%%+ *% =#%%%%*    \n" +
+                            "            %%-           \n" +
+                            "           *%%#           \n";
+
+    // Heart Club
+    String heartClub =
+                            "                          \n" +
+                            "                          \n" +
+                            "      -*%%=.   .=%%*-     \n" +
+                            "    *%%%%%%%**%%%%%%%*    \n" +
+                            "   :%%%%%%%%%%%%%%%%%%+   \n" +
+                            "   -%%%%%%%%%%%%%%%%%%*   \n" +
+                            "    :%%%%%%%%%%%%%%%%=    \n" +
+                            "      %%%%%%%%%%%%%%.     \n" +
+                            "       #%%%%%%%%%%#       \n" +
+                            "        :%%%%%%%%-        \n" +
+                            "          +%%%%+          \n" +
+                            "            **            \n";
+
+    // Spade Club
+    String spadeClub =
+                            "                          \n" +
+                            "                          \n"+
+                            "            *#:           \n" +
+                            "         .%%%%%%+         \n" +
+                            "       :#%%%%%%%%%+       \n" +
+                            "      *%%%%%%%%%%%%%:     \n" +
+                            "    -%%%%%%%%%%%%%%%%+    \n" +
+                            "   -%%%%%%%%%%%%%%%%%%+   \n" +
+                            "    #%%%%%%%%%%%%%%%%%.   \n" +
+                            "     =#%%%* #% *%%%%*     \n" +
+                            "            %%.           \n" +
+                            "           *%%#           \n";
+
 
 
     // METHODS
@@ -65,10 +208,16 @@ public class BlackJackController {
         youBetText.setText("Your bet: " + betMoney);
     }
 
+    // Create cards clubs
+    void cardClub(TextArea card, String cardClub){
+        card.setText(cardClub);
+    }
+
     // Place Bet
     @FXML
     void placeBet(MouseEvent event) {
         placeBetScreen.setVisible(false);
+
     }
 
     // Add 10 to the bet
@@ -149,6 +298,24 @@ public class BlackJackController {
             yourMoney += 200;
             updateQuantities();
         }
+    }
+
+    // When "Hit" button is pressed
+    @FXML
+    void onHit(MouseEvent event) {
+
+    }
+
+    // When "Stand" button is pressed
+    @FXML
+    void onStand(MouseEvent event) {
+
+    }
+
+    // When "Next" button is pressed
+    @FXML
+    void onNext(MouseEvent event) {
+
     }
 
 }
